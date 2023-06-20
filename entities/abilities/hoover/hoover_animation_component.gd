8fd738fd4eaf154ed00250950e8c73ed54ce4258
zerocker <zerocloude@gmail.com>
2023-06-20 23:08:25 +0900

    feat(abilities/hoover): start implementing a spit attack for Hoover

---
+++ entities/abilities/hoover/hoover_animation_component.gd
@@ -0,0 +1,23 @@
+extends AnimationComponent
+
+
+func play_suck_animation(direction: Vector2i) -> void:
+	var facing_direction = ''
+	if direction.x != 0:
+		facing_direction = 'side'
+	if direction.y < 0:
+		facing_direction = 'up'
+	if direction.y > 0:
+		facing_direction = 'down'
+	
+	if facing_direction:
+		self.reset()
+		_player.play('hoover/suck_' + facing_direction)
+
+
+func reset_suck_animation() -> void:
+	if _player.is_playing():
+		_player.play('hoover/RESET')
+		_player.advance(0)
+		_player.stop()
+	self.play_idle()
---
+++ entities/abilities/hoover/hoover_particle_emiiter.gd
@@ -0,0 +1,3 @@
+class_name HooverParticleEmitter
+extends Node2D
+
