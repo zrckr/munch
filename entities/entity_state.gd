32800a3e0da27dea237e39102e1097efe76c3cbf
zerocker <zerocloude@gmail.com>
2023-07-12 12:11:46 +0900

    feat(entities): add the actions stack for EntityState

--- entities/entity_state.gd
+++ entities/entity_state.gd
@@ -2,26 +2,56 @@
 class_name EntityState
 extends CanvasItem
 
+signal action_changed(previous, next)
+
+@export
+var initial_action: EntityAction
+
 @onready
 var entity := owner as Entity:
 	get: return entity
 
 var action: StringName:
-	get:
-		return action
-	set(value):
-		if action != value:
-			last_action = action
-			action = value
+	get: return _actions_stack.front()
+	set(value): transition_to(value)
 
-var last_action: StringName
+var last_action: StringName:
+	get: return _actions_stack[1] if len(_actions_stack) > 1 else &''
+
+var _actions_stack: Array[StringName] = [&'']:
+	get: return _actions_stack
 
 
-func get_actions() -> Array[EntityAction]:
-	var actions: Array[EntityAction] = []
-	for node in get_children():
-		if node is EntityAction:
-			actions.push_back(node)
-		else:
-			push_warning("'%s' is not an EntityAction" % node.get_path())
-	return actions
+func _ready() -> void:
+	if not initial_action:
+		push_warning('Initial action is not defined for %s' % entity.name)
+	else:
+		transition_to(initial_action.name)
+
+
+func transition_to(next_action: StringName) -> void:
+	if next_action != action:
+		var previous_action = action
+		_actions_stack[0] = next_action
+		action_changed.emit(previous_action, next_action)
+
+
+func transition_queue(next_action: StringName) -> void:
+	if next_action != action:
+		var previous_action = action
+		_actions_stack.push_front(next_action)
+		action_changed.emit(previous_action, next_action)
+
+
+func transition_back() -> void:
+	if len(_actions_stack) < 2:
+		push_error('There are no previous actions in the stack in the %s' % entity.name)
+		return
+	
+	var previous_action = action
+	_actions_stack.pop_front()
+	action_changed.emit(previous_action, action)
+
+
+func reset() -> void:
+	_actions_stack.clear()
