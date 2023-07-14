7cbd70671bcd5b31736916f79b1524b5dc83c242
zerocker <zerocloude@gmail.com>
2023-07-14 16:44:16 +0900

    feat(addons/fsm): implement addons for creating FSMs

---
+++ addons/fsm/nodes/state.gd
@@ -0,0 +1,70 @@
+## Abstract class. Extend it to create own states.
+## A [State] can be attached to a [StateMachine] by adding it as a child node to the [StateMachine] node.
+@icon("../icons/state.svg")
+class_name State
+extends Node
+
+## If [code]true[/code], this state will not be processed by the state machine.
+@export
+var disabled := false:
+	get:
+		return disabled
+	set(value):
+		disabled = value
+		_was_active = not value
+		set_physics_process(not value)
+
+## Reference to the state machine. Use inside state classes to
+## transition between states.
+var state_machine: StateMachine
+
+var _was_active := false
+
+
+func _physics_process(delta: float) -> void:
+	transition_attempts()
+	var is_active = is_state_active(state_machine.current_state)
+	
+	if is_active:
+		if not _was_active:
+			var kwargs = state_machine.consume_kwargs(self)
+			begin(kwargs)
+		else:
+			act(delta)
+	elif _was_active:
+		end()
+	
+	_was_active = is_active
+
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_READY:
+			if not Engine.is_editor_hint() and is_inside_tree():
+				set_physics_process(not disabled)
+
+
+## Used by [StateMachine] every process frame to
+## perform a transition to this state.
+func transition_attempts() -> void:
+	pass
+
+
+## Used by [StateMachine] to enter this state.
+func begin(_kwargs := {}) -> void:
+	pass
+
+
+## Used by [StateMachine] to tick this state.
+func act(_delta: float) -> void:
+	pass
+
+
+## Used by [StateMachine] to exit this state.
+func end() -> void:
+	pass
+
+
+## Used by [StateMachine] to evaluate this state.
+func is_state_active(current_state: State) -> bool:
+	return current_state == self
---
+++ addons/fsm/nodes/state_machine.gd
@@ -0,0 +1,115 @@
+## Manages the transition and delegation of states.
+## To use a [StateMachine] on a [Node] just add [StateMachine] as a child node.
+## To add states to the [StateMachine] extend the [State] class,
+## and add your states as child nodes to the [StateMachine] node.
+@tool
+@icon("../icons/state_machine.svg")
+class_name StateMachine
+extends Node
+
+## Emitted when the transition to a new state is done successfully.
+## Contains the [member Node.name] of the target node of the transition.
+signal transitioned(previous_state_name, current_state_name)
+
+## Points to the node representing the inital active state.
+@export
+var initial_state := NodePath():
+	get:
+		return initial_state
+	set(value):
+		initial_state = value
+		update_configuration_warnings()
+
+## The current state.
+var current_state: State:
+	get:
+		return states.front()
+
+## The states stack.
+var states: Array[State] = [null]:
+	get:
+		return states
+
+var _state_kwargs := {}
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: PackedStringArray = []
+	
+	if not has_node(initial_state):
+		warnings.append("The initial state for this state machine is not defined.")
+		
+	if get_children().filter(func(x): return x is State).size() < 2:
+		warnings.append("The state machine should have at least two States. Otherwise it is not useful.")
+	
+	return warnings
+
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	
+	if not has_node(initial_state):
+		push_warning("The initial state for this state machine is not defined.")
+	else:
+		states[0] = get_node(initial_state)
+	
+	await owner.ready
+	for child in get_children():
+		if child is State:
+			child.state_machine = self
+
+
+## Provides kwargs for provided state to consume.
+func consume_kwargs(state: State) -> Dictionary:
+	if not _state_kwargs.has(state):
+		return {}
+	
+	var kwargs = _state_kwargs[state.name]
+	_state_kwargs.erase(state.name)
+	return kwargs
+
+
+## Replaces the front current state with new one via its name [param state_name].
+## Optionally, takes a [param kwargs] dictionary to pass data to the state.
+func transition_to(state_name: StringName, kwargs := {}) -> void:
+	if states.is_empty():
+		transition_queue(state_name, kwargs)
+		return
+	
+	var previous_state_name = current_state.name
+	states.pop_front()
+	states.push_front(get_node(NodePath(state_name)))
+	
+	transitioned.emit(previous_state_name, state_name)
+	_state_kwargs[state_name] = kwargs
+
+
+## Pushes down the front current state with new one via its name [param state_name].
+## Optionally, takes a [param kwargs] dictionary to pass data to the state.
+func transition_queue(state_name: StringName, kwargs := {}) -> void:
+	var previous_state_name = current_state.name
+	states.push_front(get_node(NodePath(state_name)))
+	
+	transitioned.emit(previous_state_name, state_name)
+	_state_kwargs[state_name] = kwargs
+
+
+## Pops back the front current state with underlying one in the state stack.
+## Optionally, takes a [param kwargs] dictionary to pass data to the state.
+func transition_back(kwargs := {}) -> void:
+	if len(states) < 2:
+		push_error("There is no underlying state in the state stack for the transition")
+		return
+	
+	var previous_state_name = current_state.name
+	states.pop_front()
+	
+	transitioned.emit(previous_state_name, current_state.name)
+	_state_kwargs[current_state.name] = kwargs
+
+
+## Resets the state machine.
+func reset() -> void:
+	if current_state:
+		current_state.end()
