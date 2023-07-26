0a4446e1f6c32c2dd0befc7664412045ef094017
zerocker <zerocloude@gmail.com>
2023-07-26 11:15:16 +0900

    refactor!(entities): add property for handling directional animations
    BREAKING CHANGES:
    * The method `_play_directional_animation` has been removed.
      Use the `play` method with the `direction` property instead.

--- entities/entity_animations.gd
+++ entities/entity_animations.gd
@@ -6,19 +6,42 @@ signal started(name)
 
 signal finished(name)
 
-@onready
-var entity := owner as Entity:
-	get: return entity
+var direction := Vector2.DOWN:
+	get: return direction
+	set(value): direction = value.normalized().round()
+
+var directional_suffix: String:
+	get: return (
+		'_side' if not is_zero_approx(direction.x)
+		else '_up' if direction.y < 0
+		else '_down' if direction.y > 0
+		else ''
+	)
+
+var is_opposite_side: bool:
+	get: return not is_zero_approx(direction.x) and direction.x > 0
 
 @onready
 var _sprite: Sprite2D = $Sprite
 
 @onready
-var _animation_player: AnimationPlayer = $Player
+var _animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func play(anim_name: StringName, custom_speed := 1.0) -> void:
+	var anim_suffixed = '%s%s' % [anim_name, directional_suffix]
+	
+	if _animation_player.has_animation(anim_suffixed):
+		_sprite.flip_h = is_opposite_side
+		_animation_player.play(anim_suffixed, -1, custom_speed)
+	else:
+		_sprite.flip_h = false
+		_animation_player.play(anim_name, -1, custom_speed)
 
 
 func reset() -> void:
-	_animation_player.play('RESET')
+	_sprite.flip_h = false
+	_animation_player.play(&'RESET')
 
 
 func blink(time_sec: float) -> void:
@@ -33,17 +56,14 @@ func blink(time_sec: float) -> void:
 	visible = true
 
 
-func _play_directional_animation(anim_name: String, direction: Vector2i) -> void:
-	var facing_direction = (
-		'_side' if direction.x != 0
-		else '_up' if direction.y < 0
-		else '_down' if direction.y > 0
-		else ''
-	)
-	
-	if facing_direction:
-		_sprite.flip_h = direction.x != 0 and direction.x > 0
-		_animation_player.play(anim_name + facing_direction)
+func modulate_lerp(alpha: float, weight: float) -> void:
+	_sprite.modulate.a = lerpf(_sprite.modulate.a, alpha, weight)
+
+
+func look_at_lerp(point: Vector2, weight: float) -> void:
+	var angle_offset = (to_local(point) * scale).angle()
+	angle_offset += PI / 2.0
+	rotation = lerp_angle(rotation, rotation + angle_offset, weight)
 
 
 func _on_animation_player_started(anim_name: StringName) -> void:
